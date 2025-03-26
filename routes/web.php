02d0404f3086db01e1a454933530d9b6bc568153
Ford Henley <info@fordswebhelp.com>
2025-03-26 10:05:06 -0500

    setup integration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $response = Http::withToken(config('services.openai.secret'))
+    ->post('https://api.openai.com/v1/chat/completions',
+        [
+            "model" => "gpt-4o-mini",
+            "store" => true,
+            "messages" => [
+                    [
+                    "role" => "user",
+                    "content" => "write a haiku about ai"
+                    ]
+                ]
+        ])->json();
+
+        dd($response);
 });
